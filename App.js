09041cc8d02b9150410da5099119a12293f1c2e2
sajith-joel <joelsajith25@gmail.com>
2026-04-02 13:10:04 +0530

    fix expo sdk 49 compatibility2

--- App.js
+++ App.js
@@ -5,10 +5,12 @@ import { UserProvider } from './src/context/UserContext';
 import AppNavigator from './src/navigation/AppNavigator';
 import { LogBox, StatusBar } from 'react-native';
 
+// Ignore specific warnings
 LogBox.ignoreLogs([
   'Warning: ...',
   'Setting a timer',
   'AsyncStorage has been extracted from react-native core',
+  'Require cycle:',
 ]);
 
 export default function App() {
